dec302a35b97e153fa17d72aace474b2449d80bf
prabalesh <>
2024-08-23 12:15:27 +0530

    setup mongodb connection

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const _config = {
     PORT: process.env.PORT,
+    MONGO_CONN_STRING: process.env.MONGO_CONN_STRING as string,
 };
 
 export const config = Object.freeze(_config);
--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import app from "./backend/app";
 import { config } from "./backend/config/config";
+import connectDB from "./backend/db/dbConn";
 
 const startServer = async () => {
+    connectDB();
     app.listen(config.PORT, () => {
         console.log("Server listening on PORT: ", config.PORT);
     });
