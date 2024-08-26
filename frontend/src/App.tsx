7fec31a94d81981597eefb5195a251e52b868490
prabalesh <>
2024-08-26 23:55:26 +0530

    add notfound page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </Layout>
         </div>
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-4xl">404 - Page Not Found</h1>
+            <p className="text-lg mt-4">
+                The page you are looking for does not exist.
+            </p>
+        </div>
+    );
+};
+
+export default NotFound;
