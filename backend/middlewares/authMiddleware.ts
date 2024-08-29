837138982c2cdd339896531c5c1ce3daa17c3aaf
prabalesh <>
2024-08-29 13:20:07 +0530

    - fix bugs and refracotr authenticateToken middleware

--- backend/middlewares/authMiddleware.ts
+++ backend/middlewares/authMiddleware.ts
@@ -12,108 +12,104 @@ interface UserPayload {
 interface AuthenticatedRequest extends Request {
     user?: UserPayload;
 }
+interface User {
+    id: string;
+    name: string;
+    email: string;
+}
+
+const verifyToken = async (token: string, secret: string) => {
+    try {
+        const decoded = jwt.verify(token, secret);
+        if (typeof decoded === "object" && "id" in decoded) {
+            return decoded;
+        }
+        throw new Error("Invalid token");
+    } catch {
+        throw new Error("Token expired or invalid");
+    }
+};
+
+const generateToken = (user: User, secret: string, expiresIn: string) => {
+    return jwt.sign(user, secret, { expiresIn });
+};
 
 const authenticateToken = async (
     req: AuthenticatedRequest,
     res: Response,
     next: NextFunction
 ) => {
-    // Extract tokens from cookies
     const accessToken = req.cookies["accessToken"];
     const refreshToken = req.cookies["refreshToken"];
 
-    if (accessToken) {
-        try {
-            const decoded = jwt.verify(accessToken, config.JWT_SECRET);
-
-            if (typeof decoded === "object" && "id" in decoded) {
-                const user = await User.findById(decoded["id"]);
-                if (!user) {
-                    return res.status(403).json({
-                        success: false,
-                        message: "Invalid refresh token",
-                    });
-                }
-
-                req.user = {
-                    id: user._id.toString(),
-                    name: user.name,
-                    email: user.email,
-                };
-                user.lastLogin = new Date();
-
-                await user.save();
-            } else {
-                return res
-                    .status(403)
-                    .json({ success: false, message: "Invalid access token" });
+    try {
+        if (accessToken) {
+            const decoded = await verifyToken(accessToken, config.JWT_SECRET);
+            const user = await User.findById(decoded["id"]);
+            if (!user) {
+                throw new Error("User not found");
             }
-            req.user = decoded as UserPayload;
+            req.user = {
+                id: user._id.toString(),
+                name: user.name,
+                email: user.email,
+            };
+            user.lastLogin = new Date();
+            await user.save();
             return next();
-        } catch (err) {
-            if (err instanceof jwt.TokenExpiredError && refreshToken) {
-                try {
-                    const decodedRefreshToken = jwt.verify(
-                        refreshToken,
-                        config.JWT_SECRET
-                    );
-                    if (
-                        typeof decodedRefreshToken === "object" &&
-                        "id" in decodedRefreshToken
-                    ) {
-                        const user = await User.findById(
-                            decodedRefreshToken.id
-                        );
-                        if (!user) {
-                            return res.status(403).json({
-                                success: false,
-                                message: "Invalid refresh token",
-                            });
-                        }
-
-                        const newAccessToken = jwt.sign(
-                            {
-                                id: user._id,
-                                name: user.name,
-                                email: user.email,
-                            },
-                            config.JWT_SECRET,
-                            { expiresIn: "15min" }
-                        );
-                        user.lastLogin = new Date();
-                        await user.save();
-
-                        res.cookie("accessToken", newAccessToken, {
-                            httpOnly: true,
-                            secure: config.NODE_ENV === "production",
-                            sameSite: "none",
-                            expires: new Date(Date.now() + 15 * 60 * 1000),
-                        });
-
-                        req.user = {
-                            id: user._id.toString(),
-                            name: user.name,
-                            email: user.email,
-                        };
-                        return next();
-                    } else {
-                        throw new Error("Token expired!");
-                    }
-                } catch {
-                    return res.status(403).json({
-                        success: false,
-                        message: "Token expired!",
-                    });
-                }
-            }
-            return res
-                .status(403)
-                .json({ success: false, message: "Invalid access token" });
         }
+
+        // Handle case where access token is missing but refresh token is present
+        if (refreshToken) {
+            const decodedRefreshToken = await verifyToken(
+                refreshToken,
+                config.JWT_REFRESH_SECRET
+            );
+            const userDoc = await User.findById(decodedRefreshToken.id);
+            if (!userDoc) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Invalid refresh token",
+                });
+            }
+            const user = {
+                id: userDoc._id.toString(),
+                email: userDoc.email,
+                name: userDoc.name,
+            };
+
+            const newAccessToken = generateToken(
+                user,
+                config.JWT_SECRET,
+                "15min"
+            );
+            userDoc.lastLogin = new Date();
+            await userDoc.save();
+
+            res.cookie("accessToken", newAccessToken, {
+                httpOnly: true,
+                secure: config.NODE_ENV === "production",
+                sameSite: "none",
+                expires: new Date(Date.now() + 15 * 60 * 1000),
+            });
+
+            req.user = {
+                id: userDoc._id.toString(),
+                name: user.name,
+                email: user.email,
+            };
+            return next();
+        }
+
+        return res.status(401).json({
+            success: false,
+            message: "Login to continue",
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(403);
+        // .json({success: false, message: error.message});
     }
-    return res
-        .status(401)
-        .json({ success: false, message: "Login in to continue" });
 };
 
 export default authenticateToken;
