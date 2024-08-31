9f4b21414778dad291b13c6c4aa8cf789e0408ac
prabalesh <>
2024-08-31 15:10:34 +0530

    - update usermodel for username field - add generate username function

--- backend/auth/UserModel.ts
+++ backend/auth/UserModel.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
+const generateUsername = (name: string) => {
+    const randomNum = Math.floor(1000 + Math.random() * 9000);
+    return `${name.replace(/\s+/g, "").toLowerCase()}${randomNum}`;
+};
+
 const userSchema = new mongoose.Schema(
     {
         name: {
@@ -15,9 +20,13 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
             unique: true,
         },
+        username: {
+            type: String,
+            unique: true,
+        },
         googleID: {
             type: String,
-            required: [true, "googeID is required"],
+            required: [true, "googleID is required"],
             unique: true,
         },
         lastLogin: {
@@ -27,5 +36,21 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.username) {
+        let username = generateUsername(this.name);
+        let userExists = await mongoose.models.User.exists({ username });
+
+        while (userExists) {
+            username = generateUsername(this.name);
+            userExists = await mongoose.models.User.exists({ username });
+        }
+
+        this.username = username;
+    }
+
+    next();
+});
 
 export default mongoose.model("User", userSchema);
