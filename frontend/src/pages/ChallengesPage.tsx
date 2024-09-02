9739f0bc25f6da2abf3af633ae92eb02a5de26f9
prabalesh <>
2024-09-02 12:39:28 +0530

    ui update
    - updated challenge page ui
    - fix bugs in ui

--- frontend/src/pages/ChallengesPage.tsx
+++ frontend/src/pages/ChallengesPage.tsx
@@ -78,9 +78,9 @@ const ChallengesPage: React.FC = () => {
     }
 
     return (
-        <div className="p-4">
+        <div className="p-4 text-center">
             <h2 className="text-2xl font-bold mb-4">Challenges Faced</h2>
-            <div className="mb-4 flex gap-4">
+            <div className="mb-4 flex flex-wrap gap-2 md:gap-4">
                 <button
                     onClick={() => setFilter("all")}
                     className="bg-[--highlighted-color] px-2 py-1"
@@ -121,66 +121,101 @@ const ChallengesPage: React.FC = () => {
 
             {challenges.length > 0 ? (
                 <ul className="space-y-4">
-                    {challenges.map((challenge) => (
-                        <li
-                            key={challenge._id}
-                            className={`border p-4 rounded-xl shadow ${
-                                challenge.challenger._id === userID &&
-                                "bg-[--highlighted-color]"
-                            }`}
-                        >
-                            <h3 className="text-xl font-semibold">
-                                {challenge.challenger._id === userID
-                                    ? `You challenged ${challenge.challengedFriend.name}`
-                                    : `Challenged by ${challenge.challenger.name}`}
-                                <span>
-                                    {" "}
-                                    {challenge.winner &&
-                                    challenge.winner._id === userID ? (
-                                        <span className="text-green-500">
-                                            (You Won!)
-                                        </span>
-                                    ) : (
-                                        <span className="text-red-500">
-                                            (You Lost!){" "}
+                    {challenges.map((challenge) => {
+                        const isChallenger =
+                            challenge.challenger._id === userID;
+                        const myResult = isChallenger
+                            ? challenge.typingTestResult
+                            : challenge.friendTestResult;
+                        const opponentResult = isChallenger
+                            ? challenge.friendTestResult
+                            : challenge.typingTestResult;
+
+                        return (
+                            <li
+                                key={challenge._id}
+                                className={`border p-4 rounded-xl shadow ${
+                                    isChallenger && "bg-[--highlighted-color]"
+                                }`}
+                            >
+                                <h3 className="text-xl font-semibold">
+                                    {isChallenger
+                                        ? `You challenged ${challenge.challengedFriend.name}`
+                                        : `Challenged by ${challenge.challenger.name}`}
+
+                                    {challenge.status === "completed" && (
+                                        <span>
+                                            {" "}
+                                            {challenge.winner &&
+                                            challenge.winner._id === userID ? (
+                                                <span className="text-green-500">
+                                                    (You Won!)
+                                                </span>
+                                            ) : (
+                                                <span className="text-red-500">
+                                                    (You Lost!)
+                                                </span>
+                                            )}
                                         </span>
                                     )}
-                                </span>
-                            </h3>
-                            <p>
-                                Challenge Date:{" "}
-                                {formatRelativeTime(challenge.challengeDate)}
-                            </p>
-                            <p>Status: {challenge.status}</p>
-                            {challenge.status === "completed" && (
-                                <>
-                                    <p>Winner: {challenge.winner?.name}</p>
-                                    <p>
-                                        Your WPM:{" "}
-                                        {challenge.friendTestResult?.wpm ||
-                                            "N/A"}
-                                        , Accuracy:{" "}
-                                        {challenge.friendTestResult?.accuracy ||
-                                            "N/A"}
-                                        %
-                                    </p>
-                                    <p>
-                                        {challenge.challenger.name}'s WPM:{" "}
-                                        {challenge.typingTestResult.wpm},
-                                        Accuracy:{" "}
-                                        {challenge.typingTestResult.accuracy}%
-                                    </p>
-                                    <p>
-                                        Completed Date:{" "}
-                                        {challenge.completedDate &&
-                                            formatRelativeTime(
-                                                challenge.completedDate
-                                            )}
-                                    </p>
-                                </>
-                            )}
-                        </li>
-                    ))}
+                                </h3>
+                                <p>
+                                    Challenged:{" "}
+                                    {formatRelativeTime(
+                                        challenge.challengeDate
+                                    )}
+                                </p>
+                                <p>Status: {challenge.status}</p>
+                                {challenge.status === "completed" && (
+                                    <>
+                                        <p className="font-bold">
+                                            Winner: {challenge.winner?.name}
+                                        </p>
+                                        <div className="flex justify-between gap-2">
+                                            <div>
+                                                <p className="font-bold text-md">
+                                                    You
+                                                </p>
+                                                <p>
+                                                    WPM:{" "}
+                                                    {myResult?.wpm || "N/A"}
+                                                </p>
+                                                <p>
+                                                    Accuracy:{" "}
+                                                    {myResult?.accuracy ||
+                                                        "N/A"}
+                                                    %
+                                                </p>
+                                            </div>
+                                            <div>
+                                                <p className="font-bold text-md">
+                                                    Opponent
+                                                </p>
+                                                <p>
+                                                    WPM:{" "}
+                                                    {opponentResult?.wpm ||
+                                                        "N/A"}
+                                                </p>
+                                                <p>
+                                                    Accuracy:{" "}
+                                                    {opponentResult?.accuracy ||
+                                                        "N/A"}
+                                                    %
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <p>
+                                            Completed:{" "}
+                                            {challenge.completedDate &&
+                                                formatRelativeTime(
+                                                    challenge.completedDate
+                                                )}
+                                        </p>
+                                    </>
+                                )}
+                            </li>
+                        );
+                    })}
                 </ul>
             ) : (
                 <p>No challenges found for this filter.</p>
